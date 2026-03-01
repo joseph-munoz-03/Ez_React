1b1a5821efca131938166c0e9dda15236eae2c45
joseph-munoz-03 <jmunozp37@gmail.com>
2026-03-01 07:27:27 -0500

    Add files via upload

---
+++ Ez/src/main/java/com/example/Ez/EzApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzApplication.class, args);
+	}
+
+}
